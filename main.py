726fed600c18e4bc7b3d00345b5312feef649dd0
vliubko <vliubko@student.unit.ua>
2018-04-24 18:01:08 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    hello = b"<center>Hello from MacPaw<br><br><br><button>Get some info<br> about Vadym Liubko</button></center>"
+    hello = b"<center>Hello from MacPaw<br><br><br><a href=\"http://google.com\"><div>Hello world</div></a></center>"
     return [hello]
