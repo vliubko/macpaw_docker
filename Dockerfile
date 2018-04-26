b441426b59ab3a9313de5ce76e83be6747fafcc5
vliubko <vliubko@student.unit.ua>
2018-04-26 17:42:19 +0300

    Docker file change

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,17 @@ LABEL maintainer="vliubko@student.unit.ua"
 
 # 1) install additional system packages
 RUN		apt-get update -y
-RUN		apt-get install -y zip unzip
+RUN		apt-get install -y zip unzip logrotate
+RUN 	pip install --upgrade pip
+RUN		pip install flask
+RUN 	pip3 install urllib3
+
 #RUN		apt-get install -y vim
 
 # 2) exposing port 80
 EXPOSE 80/tcp
 
-
-# 3, 4, 5) Task to change config files. Two methods below
-
+# 3,4,5) Task to change config files. Two methods below
 # method #1: using sed replace
 # set correct application at uwsgi.ini file to remove Internal Server Error
 RUN		sed -i "s|wsgi-file=/app/wrong.py|wsgi-file=/app/main.py |g" /app/uwsgi.ini
@@ -24,9 +26,24 @@ COPY	nginx.conf /etc/nginx/conf.d/
 # change pyhton script to return HTML code with info about author
 COPY	main.py /app/
 
-
 # 6) finding password for zip file
-
+# you can find zip password in file /app/zip_pass.txt
 COPY		zip_pass_vliubko.sh /
 RUN 		chmod +x /zip_pass_vliubko.sh
 RUN 		/zip_pass_vliubko.sh
+
+### BONUS TASKS ###
+
+# 7) add log rotation for supervisor and dpkg files at /var/log/ directory
+
+# delete olg config for dpkg logrotate, copy my config files in container
+RUN			rm -f /etc/logrotate.d/dpkg
+COPY		logrotate_supervisor /etc/logrotate.d
+COPY		logrotate_dpkg /etc/logrotate.d
+
+# debug logrotate and force run logrotate
+RUN			logrotate -d /etc/logrotate.d/ ; logrotate -v -f /etc/logrotate.d/ 2>/dev/null
+
+# 8) create application, shows IP address at internship.macpaw.io/ip
+
+COPY 		index.html /app
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+import uwsgi
+from flask import Flask
+from urllib.request import urlopen
+
+app = Flask(__name__)
+my_ip = urlopen('http://ip.42.pl/raw').read()
+
+
+# @app.route('/', methods=['GET'])
+# def application(env, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/html')])
+#     hello = b"<style>@import url('https://fonts.googleapis.com/css?family=Fredoka+One');</style><div style='background-color:rgb(26,26,26);font-family: monospace;font-size: 20px;line-height: 1.2;letter-spacing: 3px;font-weight: 400;height:100%''><img src='https://cdn.macpaw.com/media/78f0d1ab64709f0e5c09d4fedbb59f54/img/internship2018/macpaw-logo@2x.png'><center><font color=white>Hello from MacPaw</font><br><br><br><a href=\"http://vliubko.synology.me\" style='background-color: #2fba6d; color: white; display: block; font-family:\"Fredoka One\", sans-serif; border-radius: 20px; padding: 10px; text-decoration: none; width: 190px; text-align: center;'>INFO<br> VADYM LIUBKO</a></center></div>"
+#     return [hello]
+
+@app.route('/ip', methods=['GET'])
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    hello = b"<style>@import url('https://fonts.googleapis.com/css?family=Fredoka+One');</style><div style='background-color:rgb(26,26,26);font-family: monospace;font-size: 20px;line-height: 1.2;letter-spacing: 3px;font-weight: 400;height:100%''><img src='https://cdn.macpaw.com/media/78f0d1ab64709f0e5c09d4fedbb59f54/img/internship2018/macpaw-logo@2x.png'><center><font color=white>Hello from MacPaw</font><br><br><br><a href=\"http://vliubko.synology.me\" style='background-color: #2fba6d; color: white; display: block; font-family:\"Fredoka One\", sans-serif; border-radius: 20px; padding: 10px; text-decoration: none; width: 190px; text-align: center;'>INFO<br> VADYM LIUBKO</a></center></div>"
-    return [hello]
+	start_response('200 OK', [('Content-Type', 'text/html')])
+	css = b"<style>@import url('https://fonts.googleapis.com/css?family=Fredoka+One');</style>"
+	css = css + b"<style>body{background-color:rgb(26,26,26); color: white; font-family: monospace; font-size: 20px;}"
+	button_css = b"#ip{background-color: #2fba6d; color: white; display: block;	font-family: 'Fredoka One', sans-serif;"
+	button_css = button_css + b"border-radius: 20px; padding: 10px;	text-decoration: none; width: 190px; text-align: center;"
+	string = b"<center><div style='margin-top: 20%'>Your ip is: </div><br>"
+	button = b"<div id ='ip'>"
+
+	return [css + button_css + b"</style>" + string + button + my_ip + b"</div></center>"]
+
+if __name__ == "__main__":
+   app.run()
